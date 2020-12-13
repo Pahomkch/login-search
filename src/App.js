608d059e0739bb6a51a290f8182a23fbcacb7f23
Pahomkch <Pahomov.kch@gmail..com>
2020-12-13 16:28:16 +0500

    fix bug. add material ui icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import './App.css';
+import React from "react";
+import {Switch, Route} from "react-router-dom";
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+import {login, logout} from "./Redux/AuthReducer";
+import {
+  getUserName,
+  getUserToken,
+  getIsAuthUser,
+  getErrorText,
+  getUsersFromState,
+} from "./Redux/Selectors/selectors";
+import {getUsers} from "./Redux/UsersReducer";
+import Login from "./Components/Login/Login";
+import Users from "./Components/Users";
+import {makeStyles} from "@material-ui/core";
 
-function App() {
+const useStyles = makeStyles({
+  app: {
+    height: "90vh",
+    margin: 10,
+    display: "flex",
+    justifyContent: "center",
+  },
+});
+function App({login, isAuth, errorText, logout, users, getUsers}) {
+  const classes = useStyles();
   return (
-    <div className="App">
-      install all packages
+    <div className={classes.app}>
+      <Switch>
+        <Route path="/login-form">
+          <Login login={login} isAuth={isAuth} errorText={errorText} />
+        </Route>
+        <Route path="/">
+          <Users isAuth={isAuth} logout={logout} users={users} getUsers={getUsers} />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  userName: getUserName(state),
+  token: getUserToken(state),
+  errorText: getErrorText(state),
+  isAuth: getIsAuthUser(state),
+  users: getUsersFromState(state),
+});
+
+App.propTypes = {
+  isAuth: PropTypes.bool,
+  errorText: PropTypes.string,
+  users: PropTypes.array,
+  login: PropTypes.func,
+  logout: PropTypes.func,
+  getUsers: PropTypes.func,
+};
+
+export default connect(mapStateToProps, {login, logout, getUsers})(App);
