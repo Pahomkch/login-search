45c534eddbac74e8875c5e045641cf5851b789d5
Pahomkch <Pahomov.kch@gmail..com>
2020-12-13 16:35:07 +0500

    change index.js. Add Provider and BrowserRouter for navigation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./Redux/store";
+
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
 
